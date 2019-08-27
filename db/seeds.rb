c6e900361630c1f2620fb56f27cf0e680ddadbea
Lucas del Rio <lucasgdelrio93@gmail.com>
2019-08-27 12:16:21 -0300

    Adds db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,15 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(name: 'Lucas', username: 'lucdelrio', email: 'lucas.delrio@gmail.com', password: 'lucas123456')
+User.create(name: 'Emanuel', username: 'manuginobili', email: 'manuginobili@sanantonio.com', password: 'manuginobili04')
+
+BankAccount.create(name: 'Santander', user_id: 1)
+
+Currency.create(name: 'dolar estadounidense', bank_account_id: 1)
+
+BankAccount.create(name: 'Francés', user_id: 2)
+Currency.create(name: 'dolar australiano', bank_account_id: 2)
